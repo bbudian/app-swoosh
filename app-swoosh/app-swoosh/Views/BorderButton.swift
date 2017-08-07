d4c756c93975ffb1dbb5e87c4b4b4e605f174d2f
Ben <bbudian0@gmail.com>
2017-08-07 12:03:23 -0400

    Setting up the welcome screen

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Benjamin Budian on 8/7/17.
+//  Copyright © 2017 ThisGuyMagic. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
